17d4b85b3195e17abba333855313969358e2c2bc
phchu <phchu@192.168.219.139>
2022-12-28 22:34:12 +0900

    test

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author phchu
+ *
+ */
+module testpro {
+}
---
+++ src/testpack/testjava.java
@@ -0,0 +1,10 @@
+package testpack;
+
+public class testjava {
+
+	public static void main(String[] args) {
+		System.out.println("Hellowold");
+
+	}
+
+}
